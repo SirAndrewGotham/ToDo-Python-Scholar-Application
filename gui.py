372cb4b1c5e38aabfc023ae442269caab86bca5a
Andrew Gotham <andreogotema@gmail.com>
2025-06-07 19:01:49 +0300

    add a notification popup to empty add/save actions and try-except to complete action

--- gui.py
+++ gui.py
@@ -33,7 +33,7 @@ while True:
     match event:
         case "add":
             if values['todo'].strip(' ') == '':
-                continue
+                sg.popup("Please write something meaningful. Empty todos are not a way to go.", font=("Helvetica", 20))
             else:
                 todos = functions.get_todos()
                 new_todo = values['todo'] + "\n"
@@ -46,7 +46,7 @@ while True:
             strip out duplicate while spaces using "re" library, get rid of the line breaks (in the middle of the file if any exists (old strings read from files have those), and add new line break at the end, then save to the file
             '''
             if values['todo'].strip(' ') == '':
-                continue
+                sg.popup("Please write something meaningful. Empty todos are not a way to go.", font=("Helvetica", 20))
             else:
                 todo_to_edit = values['todos'][0]
                 new_todo = values['todo']
@@ -58,12 +58,15 @@ while True:
                 window['todos'].update(values=todos)
                 window['todo'].update(value='')
         case "complete":
-            todo_to_complete = values['todos'][0]
-            todos = functions.get_todos()
-            todos.remove(todo_to_complete)
-            functions.write_todos(todos)
-            window['todos'].update(values=todos)
-            window['todo'].update(value='')
+            try:
+                todo_to_complete = values['todos'][0]
+                todos = functions.get_todos()
+                todos.remove(todo_to_complete)
+                functions.write_todos(todos)
+                window['todos'].update(values=todos)
+                window['todo'].update(value='')
+            except IndexError:
+                sg.popup("Please select a todo to complete first.", font=("Helvetica", 20))
         case "todos":
             window['todo'].update(value=values['todos'][0])
         case "Close":
