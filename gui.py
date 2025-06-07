4ac1668ff619f991fdb1b58b6d1ace76cd83f04a
Andrew Gotham <andreogotema@gmail.com>
2025-06-07 18:47:38 +0300

    swap pop with remove for the complete case, add keys to buttons and use in cases

--- gui.py
+++ gui.py
@@ -6,9 +6,9 @@ label = sg.Text("Type in your to-do or pick one from the list below for editing 
 # pick_label = sg.Text("Pick a todo from the list below for editing or completing, please.")
 input_box = sg.InputText(tooltip="Enter a to-do", key="todo")
 list_box = sg.Listbox(values=functions.get_todos(), key="todos", enable_events=True, size=[45, 10])
-add_button = sg.Button("Add new todo")
-save_button = sg.Button("Save changes")
-complete_button = sg.Button("Complete todo")
+add_button = sg.Button("Add new todo", key="add")
+save_button = sg.Button("Save changes", key="save")
+complete_button = sg.Button("Complete todo", key="complete")
 close_button = sg.Button("Close")
 
 layout = [
@@ -31,8 +31,8 @@ while True:
     # print(3, values["todos"])
     # print(4, values["todo"])
     match event:
-        case "Add new todo":
-            if values['todo'] == '':
+        case "add":
+            if values['todo'].strip(' ') == '':
                 continue
             else:
                 todos = functions.get_todos()
@@ -41,11 +41,11 @@ while True:
                 functions.write_todos(todos)
                 window['todos'].update(values=todos)
                 window['todo'].update(value='')
-        case "Save changes":
+        case "save":
             '''
             strip out duplicate while spaces using "re" library, get rid of the line breaks (in the middle of the file if any exists (old strings read from files have those), and add new line break at the end, then save to the file
             '''
-            if values['todo'] == '':
+            if values['todo'].strip(' ') == '':
                 continue
             else:
                 todo_to_edit = values['todos'][0]
@@ -57,11 +57,10 @@ while True:
                 functions.write_todos(todos)
                 window['todos'].update(values=todos)
                 window['todo'].update(value='')
-        case "Complete todo":
+        case "complete":
             todo_to_complete = values['todos'][0]
             todos = functions.get_todos()
-            todo_to_remove = todos.index(todo_to_complete)
-            todos.pop(todo_to_remove)
+            todos.remove(todo_to_complete)
             functions.write_todos(todos)
             window['todos'].update(values=todos)
             window['todo'].update(value='')
